51fcdf8307b5e2f1535cd34c6f3f8851546d010f
wangxiaotong <956816428@qq.com>
2019-07-30 20:51:40 +0800

    选择登陆

--- xuanze.py
+++ xuanze.py
@@ -1,9 +1,10 @@
-#from 注册 import ×
-#from 登录 import ×
-#from 取钱 import ×
-#from 存钱 import ×
+from zhuce import *
+from denglu import *
+from quqian import *
+from cunqian import *
 import csv
 flog=False
+
 def xuanze():
     print('1.注册2.登录3.存钱4.取钱5.退出')
     while True:
@@ -13,14 +14,22 @@ def xuanze():
         if choice==1:
 
             register()
-            return True
-        elif choice==2 and flog:
-            denglu()
 
-        elif choice == 3 and flog:
-            cunqian()
-        elif choice == 4 and flog:
-            quqian()
+        elif choice==2 :
+            denglu()
+            flog=True
+            return flog
+        elif choice == 3 :
+            if flog:
+                cunqian()
+            else:
+                print('未登录')
+        elif choice == 4 :
+            if flog:
+                quqian()
+            else:
+                print('未登录')
+
 
         else:
             print('退出')
