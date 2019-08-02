e8e0ce42a63dbc330525c4b8b8d420e9d71c520b
wangxiaotong <956816428@qq.com>
2019-08-02 21:26:48 +0800

    数据库

--- zhuce.py
+++ zhuce.py
@@ -1,38 +1,27 @@
 import csv
-
+import pymysql
+conn=pymysql.connect(host='49.232.42.221',port=3306,user='root',password='123456',charset='utf8',database='bank')
+curse=conn.cursor()
 
 def register():
     print('--------用户注册--------------')
     while True:
-        username = input('用户名:')
-        password = input('密码:')
+        name = input('用户名:')
+        word = input('密码:')
         repassword = input('确认密码:')
 
-        if password == repassword:
+        if word == repassword:
             try:
-                # 读取文件
-                with open(r'lib.csv', mode='r') as rstream:
-                    users = csv.reader(rstream)  # ['zhiqiang#666666\n',.....]
-                    if list(users) == []:
-                        with open(r'lib.csv', mode='w', newline='') as wstream:
-                            writer_csv = csv.writer(wstream)
-                            user = [username, password, 0]
-                            writer_csv.writerow(user)
-                    else:
-                        for user in users:
-                            uname = user[0]
-                            # print('---->uname', uname)
-                            # 进行比较判断
-                            if username == uname:
-                                print('此用户已注册！')
-                                return
-                            with open(r'lib.csv', mode='a', newline='') as wstream:
-                                writer_csv = csv.writer(wstream)
-                                user = [username, password, 0]
-                                writer_csv.writerow(user)
+                result=curse.execute('insert into inf(username,password) values (%s,%s);',(name,word))
+                conn.commit()
 
-            except:
-                print('注册失败')
+
+                # 读取文件
+
+
+            except Exception as e:
+                print('注册失败',e)
+                conn.rollback()
             else:
                 print('注册成功！')
                 break
