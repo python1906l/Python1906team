48bb33c492dd827bdd38dfbede3fdf66462db83d
wangxiaotong <956816428@qq.com>
2019-08-03 09:41:44 +0800

    登录

--- login.py
+++ login.py
@@ -3,15 +3,17 @@ import xuanze
 
 def denglu():
     print('-----用户登录------')
-    username = input('用户名:')
-    password = input('密码:')
+    name = str(input('用户名:'))
+    word = str(input('密码:'))
     import pymysql
     conn = pymysql.connect(host='49.232.42.221', port=3306, user='root', password='123456', charset='utf8',
                            database='bank')
     curse = conn.cursor()
     try:
-        result = curse.execute('select username,password,money from inf where username={} and password={};'.format(username,password))
-        print('用户信息：用户名：{}密码：{}余额：{}'.format(username,password,result[2]))
+        curse.execute("select username,password,money from inf where username='{}'and password='{}'".format(name,word))
+        user=curse.fetchone()
+
+        print('用户信息：用户名：{}密码：{}余额：{}'.format(name,word,user[2]))
 
 
 
